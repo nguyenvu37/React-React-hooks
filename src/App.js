83a35cf843a02cf425d1733dd75fc4e19123a48e
NGUYỄN ANH VŨ <nguyenanhvu.bkdn@gmail.com>
2020-04-07 21:00:13 +0700

    Add ex1 change color box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ColorBox from './components/colorBox';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React Hooks</h1>
+      <ColorBox />
     </div>
   );
 }
